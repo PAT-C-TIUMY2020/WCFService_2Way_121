3fa274352d9565c29be2c2f669d30e16a402c252
MuhammadRifal22 <rifalmuhammad797@gmail.com>
2021-01-16 19:29:43 +0700

    Add project files.

---
+++ WCFService_2Way_121/ServiceCallback.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_121
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.Single)]
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ServiceCallback : IServiceCallback
+    {
+        Dictionary<IClientCallback, string> userList = new Dictionary<IClientCallback, string>();
+        public void gabung(string username)
+        {
+            IClientCallback koneksiGabung = OperationContext.Current.GetCallbackChannel<IClientCallback>();
+            userList[koneksiGabung] = username;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public void kirimPesan(string pesan)
+        {
+            IClientCallback koneksiPesan = OperationContext.Current.GetCallbackChannel<IClientCallback>();
+            string user;
+            if (!userList.TryGetValue(koneksiPesan, out user))
+            {
+                return;
+            }
+            foreach (IClientCallback other in userList.Keys)
+            {
+                other.pesanKirim(user, pesan);
+            }
+        }
+    }
+}
